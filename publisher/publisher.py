2fa3fae03737702768306728c97cdb7872da58ef
lukezdz <lukas.zdziarski@gmail.com>
2021-11-01 22:13:33 +0100

    Configure docker-compose to run all containers automatically

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -27,43 +27,47 @@ import argparse
 # connection = pika.BlockingConnection(pika.ConnectionParameters(args.host, args.port, '/', credentials))
 # channel = connection.channel()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.com'))
-channel = connection.channel()
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.local', port=5672))
+    channel = connection.channel()
 
-while True:
-    try:
-        temp_dict = dict([(f'temp-{x + 1}', random.randrange(0, 60)) for x in range(30)])
-        hum_dict = dict([(f'hum-{x + 1}', random.randrange(0, 100)) for x in range(30)])
+    while True:
+        try:
+            temp_dict = dict([(f'temp-{x + 1}', random.randrange(0, 60)) for x in range(30)])
+            hum_dict = dict([(f'hum-{x + 1}', random.randrange(0, 100)) for x in range(30)])
 
-        config = {
-            'temp': temp_dict,
-            'humidity': hum_dict
-        }
-        print(config)
-        print(config['temp']['temp-5'])
-        print(config['humidity']['hum-10'])
+            config = {
+                'temp': temp_dict,
+                'humidity': hum_dict
+            }
+            print(config)
+            print(config['temp']['temp-5'])
+            print(config['humidity']['hum-10'])
 
-        for x in temp_dict.keys():
-            channel.basic_publish(exchange='',
-                                  routing_key='sensors-queue',
-                                  body=str.encode(x))  # sensor's id
-            channel.basic_publish(exchange='',
-                                  routing_key='sensors-queue',
-                                  body=temp_dict[x].to_bytes(1, 'little'))  # sensor's value
-            print("sensors-queue: temp message sent")
-            time.sleep(0.2)
+            for x in temp_dict.keys():
+                channel.basic_publish(exchange='',
+                                    routing_key='sensors-queue',
+                                    body=str.encode(x))  # sensor's id
+                channel.basic_publish(exchange='',
+                                    routing_key='sensors-queue',
+                                    body=temp_dict[x].to_bytes(1, 'little'))  # sensor's value
+                print("sensors-queue: temp message sent")
+                time.sleep(0.2)
 
-        for x in hum_dict.keys():
-            channel.basic_publish(exchange='',
-                                  routing_key='sensors-queue',
-                                  body=str.encode(x))  # sensor's id
-            channel.basic_publish(exchange='',
-                                  routing_key='sensors-queue',
-                                  body=hum_dict[x].to_bytes(1, 'little'))  # sensor's value
-            print("sensors-queue: humidity message sent")
-            time.sleep(0.2)
+            for x in hum_dict.keys():
+                channel.basic_publish(exchange='',
+                                    routing_key='sensors-queue',
+                                    body=str.encode(x))  # sensor's id
+                channel.basic_publish(exchange='',
+                                    routing_key='sensors-queue',
+                                    body=hum_dict[x].to_bytes(1, 'little'))  # sensor's value
+                print("sensors-queue: humidity message sent")
+                time.sleep(0.2)
 
-    except KeyboardInterrupt:
-        print('sensors-queue: closed')
-        connection.close()
-        break
+        except KeyboardInterrupt:
+            print('sensors-queue: closed')
+            connection.close()
+            break
+
+if __name__ == "__main__":
+    main()
